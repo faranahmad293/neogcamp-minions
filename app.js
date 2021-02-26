72c83fd3f4d653379ed6a73359585fcbe8f004c5
faranahmad293 <79214797+faranahmad293@users.noreply.github.com>
2021-02-26 11:28:11 +0530

    added comment of live ex

--- app.js
+++ app.js
@@ -10,3 +10,9 @@ btnTrnslate.addEventListener("click", function clickEventHandler{
    console.console.log("clicked"); 
    console.log("input", txtInput.value)
 })
+
+
+// 1. document.querySelector("textarea")
+// 2. .btn-primary
+// 3. #input-btn
+// 4. "input[name='translator']"
