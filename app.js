8b813cb450202ac374beeefd4951e187d1709bb3
faranahmad293 <79214797+faranahmad293@users.noreply.github.com>
2021-02-26 11:46:51 +0530

    input output processing done

--- app.js
+++ app.js
@@ -5,7 +5,16 @@
 // alert(welcomemessage) 
 var btnTrnslate = document.querySelector("#btn-translate") 
 var txtInput = document.querySelector("txt-input");
-console.log(txtInput)
+var outputDiv = document.querySelector("#output");
+
+
+// outputDiv.innerText = "FARAN AHMAD"
+
+outputDiv.innerText = "basushisqi " + txtInput.value
+
+console.log(outputDiv);
+
+ 
 btnTrnslate.addEventListener("click", function clickEventHandler{
    console.console.log("clicked"); 
    console.log("input", txtInput.value)
