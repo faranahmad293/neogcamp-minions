da1554266856f7b072ca6af5232ad1de67b54b6f
faranahmad293 <79214797+faranahmad293@users.noreply.github.com>
2021-02-26 11:09:47 +0530

    till taking input

--- app.js
+++ app.js
@@ -3,5 +3,10 @@
 
 // var welcomemessage = "This script works!! " + username
 // alert(welcomemessage) 
-var btnTrnslate = document.querySelector("#btn-translate")
-
+var btnTrnslate = document.querySelector("#btn-translate") 
+var txtInput = document.querySelector("txt-input");
+console.log(txtInput)
+btnTrnslate.addEventListener("click", function clickEventHandler{
+   console.console.log("clicked"); 
+   console.log("input", txtInput.value)
+})
